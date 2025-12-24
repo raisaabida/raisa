8ad3097b12f02f48673844d50d2c44e4f8bdbb51
raisaabida <231011112@eastdelta.edu.bd>
2025-12-24 21:53:03 +0600

    fifth commit

--- config/stripe.js
+++ config/stripe.js
@@ -2,7 +2,7 @@ import Stripe from "stripe";
 
 let stripe = null;
 
-// Only initialize Stripe if a valid key is provided (not placeholder)
+
 if (process.env.STRIPE_SECRET_KEY && !process.env.STRIPE_SECRET_KEY.includes("xxxxxxxxx")) {
   try {
     stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
